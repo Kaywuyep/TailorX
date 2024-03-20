ab71b4c3c6efcf36cf40da72a7973a6d097f9ff2
Kaywuyep <wuyepkate@gmail.com>
2024-03-20 14:25:00 -0700

    fix

--- TailoxProject/TailorxApp/views.py
+++ TailoxProject/TailorxApp/views.py
@@ -1,6 +1,7 @@
 #from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from django.views.generic import DetailView
+from django.shortcuts import render
 from .models import Post
 # Create your views here.
 
