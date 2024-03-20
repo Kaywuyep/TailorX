c28971a6ae95de29cea82894f25f19465d8e6950
Kaywuyep <wuyepkate@gmail.com>
2024-03-20 05:05:06 -0700

    updated app

--- TailoxProject/TailoxProject/settings.py
+++ TailoxProject/TailoxProject/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = {
-        'DIRS': [os.path.join.(BASE_DIR, 'static')],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
 }
 
 # Default primary key field type
