8c088366a748916e92ab01935cd842b31c486dfc
Catherine Wuyep <wuyepkate@gmail.com>
2024-04-15 17:50:56 +0100

    Delete TailorxProject1 directory

--- TailorxProject1/TailorxApp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class TailorxappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TailorxApp'
-
-    def ready(self):
-        import TailorxApp.signals
